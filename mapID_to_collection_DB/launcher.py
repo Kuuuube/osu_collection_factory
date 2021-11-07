214fb93511b2a4bf28062d5dbd07d326d0bb3fa0
kuuube <hexagonisalie@gmail.com>
2021-11-06 21:37:42 -0400

    implement extreme speed osu collector dumping

--- mapID_to_collection_DB/launcher.py
+++ mapID_to_collection_DB/launcher.py
@@ -8,11 +8,18 @@ import setID_to_mapIDs
 
 html_check = "0"
 osu_collector_check = "0"
+diff_filter_min = "0"
+diff_filter_max = "9999"
+
 
 osu_collector_check = input("Dump osu!Collector collection? (y/n): ")
 
 if re.search("(y|Y)", osu_collector_check) != None:
     collection_id = input("Enter collection ID or URL: ")
+    diff_filter_check = input("Filter by star rating? (y/n): ")
+    if  re.search("(y|Y)", diff_filter_check) != None:
+        diff_filter_min = input("Minimum star rating: ")
+        diff_filter_max = input("Maximum star rating: ")
 
 if  re.search("(n|N)", osu_collector_check) != None:
     html_check = input("Use a HTML/HTM file instead of list.txt (y/n): ")
@@ -20,24 +27,30 @@ if  re.search("(n|N)", osu_collector_check) != None:
     if re.search("(y|Y)", html_check) != None:
         html_path = input("Enter path to HTML/HTM file: ")
 
-api_key = input("Enter API key: ")
 collection_path = input("Enter collection DB output path: ")
 
 if re.search("(y|Y)", osu_collector_check) != None:
-    osu_collector_dumper.osu_collector_dump(collection_id)
+    if re.search("(.(d|D)(b|B))", collection_path) == None:
+        collection_path = re.sub("$", ".db", collection_path)
+    osu_collector_dumper.osu_collector_dump(collection_id, collection_path, diff_filter_min, diff_filter_max)
 
 if re.search("(y|Y)", html_check) != None:
-    html_to_list.html_to_list(html_path)
     setID_to_mapIDs.setID_to_list(html_path)
-    setID_to_mapIDs.setID_list_to_mapID_list(api_key)
+    html_to_list.html_to_list(html_path)
 
+    api_key = input("Enter API key: ")
+
+    setID_to_mapIDs.setID_list_to_mapID_list(api_key)
+    list_mapid_info_puller.id_to_db(api_key, collection_path)
+    
 if re.search("(.(d|D)(b|B))", collection_path) == None:
     collection_path = re.sub("$", ".db", collection_path)
 
 if re.search("(y|Y)", html_check) == None and re.search("(y|Y)", osu_collector_check) == None:
     setID_to_mapIDs.setID_to_list("list.txt")
     html_to_list.html_to_list("list.txt")
+
+    api_key = input("Enter API key: ")
+        
     setID_to_mapIDs.setID_list_to_mapID_list(api_key)
-
-list_mapid_info_puller.id_to_db(api_key, collection_path)
-
+    list_mapid_info_puller.id_to_db(api_key, collection_path)
--- mapID_to_collection_DB/osu_collector_dumper.py
+++ mapID_to_collection_DB/osu_collector_dumper.py
@@ -3,16 +3,24 @@ import json
 import re
 import time
 import sys
+import subprocess
 
-def osu_collector_dump(collection_id):
-
+def osu_collector_dump(collection_id, collection_path, diff_filter_min, diff_filter_max):
+    
+    collection_path_no_extension_1 = re.sub(".*(\\\|\\\\)", "", collection_path)
+    collection_path_no_extension_2 = re.sub("\..*", "", collection_path_no_extension_1)
+    
     collection_id_regex = re.search("\d*$", collection_id)
     
     hasMore = True
     cursor = "0"
     filepath = "list.txt"
+    csv_filepath = 'CollectionCSVtoDB\\' + collection_path_no_extension_2 + '.csv'
 
     with open(filepath, 'w') as id_dump:
+        id_dump.close()
+
+    with open(csv_filepath, 'w') as id_dump:
         id_dump.close()
     
     while hasMore == True:
@@ -20,13 +28,17 @@ def osu_collector_dump(collection_id):
         url = "https://osucollector.com/api/collections/" + collection_id_regex.group(0) + "/beatmapsv2?"
         payload = {
             "cursor": cursor,
-            "perPage": "100"
+            "perPage": "100",
+            "sortBy": "difficulty_rating",
+            "filterMin": diff_filter_min,
+            "filterMax": diff_filter_max
         }
 
         r = requests.get(url, payload)
         collection = json.loads(r.text)
         
-        print ("osu!Collectior: " + cursor + " waiting 5 seconds.")
+        print ("osu!Collectior: waiting 5 seconds.")
+        print (cursor)
         
         cursor = collection["nextPageCursor"]
         hasMore = collection["hasMore"]
@@ -34,10 +46,20 @@ def osu_collector_dump(collection_id):
         beatmap_list = (collection["beatmaps"])
         regex_filtered = re.findall('(?<="https://osu.ppy.sh/beatmaps/).*?(?=")', json.dumps(collection))
 
+        hashes_regex_filtered = re.findall('(?<="checksum": ").{32}', json.dumps(collection))
+        
         for item in regex_filtered:
             with open (filepath, "a") as id_dump:
                 id_dump.writelines([item])
                 id_dump.writelines(["\n"])
-            
+
+        for item in hashes_regex_filtered:
+            with open (csv_filepath, "a") as hash_dump:
+                    hash_dump.writelines([",,"])
+                    hash_dump.writelines([item])
+                    hash_dump.writelines(["\n"])
+        
         if hasMore == True:
             time.sleep(5)
+            
+    subprocess.check_call([r"CollectionCSVtoDB\CollectionCSVtoDB.exe", csv_filepath, collection_path])
