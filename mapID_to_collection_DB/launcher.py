1959e083e23ab7595f04e864ed15bfd8e27c23ba
The1Divider <kmanprovo@gmail.com>
2021-11-29 18:05:48 -0700

    Initial rewrite

--- mapID_to_collection_DB/launcher.py
+++ mapID_to_collection_DB/launcher.py
@@ -1,55 +1,96 @@
 import re
+from os import PathLike
+
 import osu_collector_dumper
 import list_mapid_info_puller
 import html_to_list
 import setID_to_mapIDs
 
-html_check = "0"
-osu_collector_check = "0"
-diff_filter_min = "0"
-diff_filter_max = "9999"
+# TODO properly handle out_collection_path
 
-osu_collector_check = input("Dump osu!Collector collection? (y/n): ")
 
-if re.search("(y|Y)", osu_collector_check) != None:
-    collection_id = input("Enter collection ID or URL: ")
-    diff_filter_check = input("Filter by star rating? (y/n): ")
-    if  re.search("(y|Y)", diff_filter_check) != None:
-        diff_filter_min = input("Minimum star rating: ")
-        diff_filter_max = input("Maximum star rating: ")
-        
-if  re.search("(n|N)", osu_collector_check) != None:
-    html_check = input("Use a HTML/HTM file instead of list.txt (y/n): ")
+def osu_collector_dump(out_collection_path: PathLike | str):
+    min_sr_filter, max_sr_filter = None, None
 
-    if re.search("(y|Y)", html_check) != None:
-        html_path = input("Enter path to HTML/HTM file: ")
+    collection_id = input("Enter collection ID or URL: ")  # TODO is there a way to validate this
 
-collection_path = input("Enter collection DB output path: ")
+    # this checks if the input is valid, if it isn't, print it and loop back
+    diff_filter = None
+    while (diff_filter := input("Filter by star rating? [y/n]: ").strip().lower()) not in {'y', 'n'}:
+        print(f"Not a valid input: {diff_filter}")
 
-if re.search("(y|Y)", osu_collector_check) != None:
-    if re.search("(.(d|D)(b|B))", collection_path) == None:
-        collection_path = re.sub("$", ".db", collection_path)
-        if re.search("(y|Y)", diff_filter_check) != None:
-            osu_collector_dumper.osu_collector_dump_diff(collection_id, collection_path, diff_filter_min, diff_filter_max)
-        if re.search("(y|Y)", diff_filter_check) == None:
-            osu_collector_dumper.osu_collector_dump(collection_id, collection_path)
-if re.search("(y|Y)", html_check) != None:
+    if diff_filter == 'y':
+        while min_sr_filter is None:
+            min_sr_filter = input("Minimum star rating: ")
+
+            try:
+                min_sr_filter = float(min_sr_filter)  # validates the filter as a float
+
+            except ValueError:
+                print(f"Invalid star rating: {min_sr_filter} - must be a number")
+                min_sr_filter = None
+
+            if min_sr_filter < 0:  # makes sure the filter is positive
+                print(f"Invalid star rating: {min_sr_filter} - must be greater or equal to 0")
+                min_sr_filter = None
+
+        while max_sr_filter is None:
+            max_sr_filter = input("Maximum star rating: ")
+
+            try:
+                max_sr_filter = float(max_sr_filter)  # validates the filter as a float
+
+            except ValueError:
+                print(f"Invalid star rating: {max_sr_filter} - must be a number")
+                max_sr_filter = None
+
+    osu_collector_dumper.osu_collector_dump(collection_id, out_collection_path, min_sr_filter, max_sr_filter)
+
+    # TODO allow osu.collector.dumper to accept diff filters with value None
+
+
+def html_dump(out_collection_path: PathLike | str):
+    html_path = input("Enter path to HTML/HTM file: ")  # TODO is there a way to validate this
     api_key = input("Enter API key: ")
+
     
     setID_to_mapIDs.setID_to_list(html_path)
     html_to_list.html_to_list(html_path)
 
     setID_to_mapIDs.setID_list_to_mapID_list(api_key)
-    list_mapid_info_puller.id_to_db(api_key, collection_path)
-    
-if re.search("(.(d|D)(b|B))", collection_path) == None:
-    collection_path = re.sub("$", ".db", collection_path)
+    list_mapid_info_puller.id_to_db(api_key, out_collection_path)
 
-if re.search("(y|Y)", html_check) == None and re.search("(y|Y)", osu_collector_check) == None:
+
+def list_txt_dump(out_collection_path: PathLike | str):
     api_key = input("Enter API key: ")
 
     setID_to_mapIDs.setID_to_list("list.txt")
     html_to_list.html_to_list("list.txt")
 
     setID_to_mapIDs.setID_list_to_mapID_list(api_key)
-    list_mapid_info_puller.id_to_db(api_key, collection_path)
+    list_mapid_info_puller.id_to_db(api_key, out_collection_path)
+
+
+if __name__ == "__main__":
+    collection_path = input("Enter collection DB output path: ")
+    if re.search(r"(\.db)", collection_path, flags=re.IGNORECASE) is None:  # had to use a raw string to avoid esc char
+        collection_path = re.sub("$", ".db", collection_path)
+
+    oc_dump = None
+    while (oc_dump := input("Dump osu!Collector collection? [y/n]: ").strip().lower()) not in {'y', 'n', ''}:
+        print(f"Not a valid input: {oc_dump}")
+
+    if oc_dump == 'y':
+        osu_collector_dump(collection_path)
+
+    else:
+        htm_dump = None
+        while (htm_dump := input("Use a HTML/HTM file instead of list.txt (y/n): ").strip().lower()) not in {'y', 'n', ''}:
+            print(f"Not a valid input: {htm_dump}")
+
+        if htm_dump == 'y':
+            html_dump(collection_path)
+
+        else:
+            print("Using list.txt...")
+            list_txt_dump(collection_path)
