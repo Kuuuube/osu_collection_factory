9c21d413ac7af47a7ebadb2aa273ea4b00fbfe0b
numpee <kmanprovo@gmail.com>
2021-12-05 02:14:57 -0700

    Fixed wrong api call being made if no filter is provided

--- mapID_to_collection_DB/osu_collector_dumper.py
+++ mapID_to_collection_DB/osu_collector_dumper.py
@@ -4,37 +4,52 @@ import time
 import subprocess
 
 
-def osu_collector_dump_diff(collection_id: int | str, collection_name: str,
-                            diff_filter_min: int | None = None, diff_filter_max: int | None = None):
+def osu_collector_dump(collection_id: int | str, collection_name: str,
+                       diff_filter_min: float | None = None, diff_filter_max: float | None = None,
+                       bpm_filter_min: int | None = None, bpm_filter_max: int | None = None):
+    # Defaults
     has_more = True
     cursor = "0"
-    filepath = "list.txt"
+    filepath = "list.txt"  # TODO send to log
+
+    # Filter booleans
+    using_diff_filter = diff_filter_min is not None or diff_filter_max is not None or diff_filter_max == 0
+    using_bpm_filter = bpm_filter_min is not None or bpm_filter_max is not None or bpm_filter_max == 0
 
     # Remove extension and parse id from url if necessary
     collection_name_no_extension = re.sub(r"\..*", "", collection_name)
-
-    if isinstance(collection_id, str):
-        collection_id = collection_id.split("/")[-1]
 
     csv_filepath = f"{collection_name_no_extension}.csv"
 
     while has_more:
         # Default url and payload
-        url = f"https://osucollector.com/api/collections/{collection_id}/beatmapsv2?"
-        payload = {
-            "cursor": cursor,
-            "perPage": "100",
-            "sortBy": "difficulty_rating"
-        }
 
-        # Filter logic
-        # TODO implement bpm min/max (can only sort by one)
-        if diff_filter_min is not None or diff_filter_max is not None:
-            payload["filterMin"] = diff_filter_min if diff_filter_min is not None else 0
-            payload["filterMax"] = diff_filter_max if diff_filter_max is not None else 0
+        if using_diff_filter:
+            url = f"https://osucollector.com/api/collections/{collection_id}/beatmapsv2?"
+            payload = {
+                "cursor": cursor,
+                "perPage": "100",
+                "sortBy": "difficulty_rating",
+                "filterMin": diff_filter_min if diff_filter_min is not None else 0,
+                "filterMax": diff_filter_max
+            }
+
+        elif using_bpm_filter:
+            url = f"https://osucollector.com/api/collections/{collection_id}/beatmapsv2?"
+            payload = {
+                "cursor": cursor,
+                "perPage": "100",
+                "sortBy": "bpm",
+                "filterMin": bpm_filter_min if bpm_filter_min is not None else 0,
+                "filterMax": bpm_filter_max
+            }
+
+        else:
+            url = f"https://osucollector.com/api/collections/{collection_id}"
+            payload = None
 
         # Make request for json
-        # TODO error handle this
+        # TODO error handle the response
         r = requests.get(url, payload)
         collection = r.json()
 
