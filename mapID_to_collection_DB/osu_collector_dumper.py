c4c50d6b15251ce3dcb27471bde76ba08fa76368
numpee <kmanprovo@gmail.com>
2021-12-06 01:57:31 -0700

    Reworked menu logic

--- mapID_to_collection_DB/osu_collector_dumper.py
+++ mapID_to_collection_DB/osu_collector_dumper.py
@@ -1,29 +1,112 @@
-import requests
+import json
 import re
-import time
 import subprocess
+from pathlib import Path
+
+from typing import NoReturn, Literal
+
+from util import get_json_response
+
+filter_menu = """\r
+1) By star rating
+2) By bpm
+"""
 
 
-def osu_collector_dump(collection_id: int | str, collection_name: str,
-                       diff_filter_min: float | None = None, diff_filter_max: float | None = None,
-                       bpm_filter_min: int | None = None, bpm_filter_max: int | None = None):
+def osu_collector_dump():
+    min_sr_filter, max_sr_filter = None, None
+    min_bpm_filter, max_bpm_filter = None, None
+
+    collection_id = input("Enter collection ID or URL: ")  # TODO is there a way to validate this
+
+    # If user wants to sort by diff
+    # noinspection PyUnusedLocal
+    use_filter = None
+    while (use_filter := input("Filter collection? [y/n]: ").strip().lower()) not in ('y', 'n'):
+        print(f"Not a valid input: {use_filter}")
+
+    if use_filter == 'y':
+        print(filter_menu)
+
+        # noinspection PyUnusedLocal
+        user_input = None
+        while (user_input := input(">")) not in ('1', '2'):
+            print(f"Invalid input: {user_input}")
+
+        match user_input:
+            case '1':
+                # Get and sanity check min sr
+                _min_sr_filter = None
+                while _min_sr_filter is None:
+                    _min_sr_filter = _filter_verification(filter_name="star rating", sort="min")
+
+                    if _min_sr_filter < 0:  # makes sure the filter is positive
+                        print(f"Invalid star rating: {_min_sr_filter} - must be greater or equal to 0")
+                        _min_sr_filter = None
+
+                min_sr_filter = _min_sr_filter
+
+                # Get and sanity check max sr
+                max_sr_filter = _filter_verification(filter_name="star rating", sort="max")
+
+            case '2':
+                # Get and sanity check min bpm
+                _min_bpm_filter = None
+                while _min_bpm_filter is None:
+                    _min_bpm_filter = _filter_verification(filter_name="bpm", sort="min")
+
+                    if _min_bpm_filter < 0:  # makes sure the filter is positive
+                        print(f"Invalid bpm: {_min_bpm_filter} - must be greater or equal to 0")
+                        _min_bpm_filter = None
+
+                min_bpm_filter = _min_bpm_filter
+
+                # Get and sanity check max bpm
+                max_bpm_filter = _filter_verification(filter_name="bpm", sort="max")
+
+    _collector_dump(collection_id, min_sr_filter, max_sr_filter, min_bpm_filter, max_bpm_filter)
+
+
+def _filter_verification(filter_name: str, sort: Literal["min"] | Literal["max"]) -> float:
+    f = None
+    while f is None:
+        f = input(f"{sort} {filter_name}: ")
+
+        if f.strip() == "":
+            f = None
+
+        try:
+            f = float(f)  # validates the filter by casting to float
+
+        except ValueError:
+            print(f"Invalid {filter_name}: {f} - must be a number")
+            f = None
+
+    return f
+
+
+def _collector_dump(collection_id: int | str,
+                    diff_filter_min: float | None = None, diff_filter_max: float | None = None,
+                    bpm_filter_min: int | None = None, bpm_filter_max: int | None = None) -> NoReturn:
     # Defaults
     has_more = True
     cursor = "0"
     filepath = "list.txt"  # TODO send to log
+
+    # Get user settings
+    with open(Path("settings.json"), 'r') as f:
+        data = json.load(f)
 
     # Filter booleans
     using_diff_filter = diff_filter_min is not None or diff_filter_max is not None or diff_filter_max == 0
     using_bpm_filter = bpm_filter_min is not None or bpm_filter_max is not None or bpm_filter_max == 0
 
     # Remove extension and parse id from url if necessary
-    collection_name_no_extension = re.sub(r"\..*", "", collection_name)
+    collection_name_no_extension = re.sub(r"\..*", "", data["collection_name"])
 
     csv_filepath = f"{collection_name_no_extension}.csv"
 
     while has_more:
-        # Default url and payload
-
         if using_diff_filter:
             url = f"https://osucollector.com/api/collections/{collection_id}/beatmapsv2?"
             payload = {
@@ -49,9 +132,7 @@ def osu_collector_dump(collection_id: int | str, collection_name: str,
             payload = None
 
         # Make request for json
-        # TODO error handle the response
-        r = requests.get(url, payload)
-        collection = r.json()
+        collection = get_json_response(url=url, payload=payload)
 
         # Collect beatmap ids and checksums from the json
         beatmap_ids = []
@@ -61,12 +142,12 @@ def osu_collector_dump(collection_id: int | str, collection_name: str,
             checksums.append(beatmap["checksum"])
 
         # Dump ids
-        with open(filepath, "a") as id_dump:
+        with open(filepath, "a") as id_dump:  # TODO log this
             for beatmap_id in beatmap_ids:
                 id_dump.write(beatmap_id + "\n")
 
         # Dump checksums
-        with open(csv_filepath, "w") as hash_dump:
+        with open(csv_filepath, "w") as hash_dump:  # TODO is csv needed to parse?
             for checksum in checksums:
                 hash_dump.write(checksum + "\n")
 
@@ -74,8 +155,6 @@ def osu_collector_dump(collection_id: int | str, collection_name: str,
         has_more = collection["hasMore"]
         if has_more:
             cursor = collection["nextPageCursor"]
-            print("osu!Collector: waiting 5 seconds.")
-            print(f"next cursor={int(cursor)}")
-            time.sleep(5)
+            print(f"next cursor={int(cursor)}")  # TODO log this
 
-    subprocess.check_call([r"CollectionCSVtoDB\CollectionCSVtoDB.exe", csv_filepath, collection_name])
+    subprocess.check_call([r"CollectionCSVtoDB\CollectionCSVtoDB.exe", csv_filepath, data["collection_name"]])
