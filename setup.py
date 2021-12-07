3ead09fcd509124f70a07a6ca9543e204fc74fd7
numpee <kmanprovo@gmail.com>
2021-12-07 04:29:41 -0700

    Rewrote setup.py to fit with __version__.py

--- setup.py
+++ setup.py
@@ -1,33 +1,41 @@
-from os import path
+from pathlib import Path
 
 from setuptools import setup
 
-with open("mapID_to_collection_DB/version.py") as f:
-    exec(f.read())
+about = {}
+with open(Path("mapID_to_collection_DB/__version__.py").resolve(), "r",  encoding="utf-8") as v:
+    exec(v.read(), about)
 
 
 def get_long_description() -> str:
-    fname = path.join(path.dirname(path.abspath(__file__)), "README.rst")
+    try:
+        fname = Path(__file__).resolve().joinpath("README.md")
+
+    except (OSError, RuntimeError):
+        raise NotImplementedError("README.md not found")
+
     with open(fname, "r") as f:
         return f.read()
 
 
+
+
 setup(
-    name="mapID_to_collection_DB",
-    description="",
+    name=about["__title__"],
+    description=about["__description__"],
     long_description=get_long_description(),
-    author="Kuuuube",
-    author_email="",
-    license="GPL-3.0 License",
-    url="",
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    license=about["__license__"],
+    url=about["__url__"],
     download_url="",
     install_requires=[
-        "requests~=2.26.0"
+        "requests"
     ],
     python_requires=">=3.10",
     project_urls={
         "Issue Tracker": "https://github.com/Kuuuube/osu_MapID_to_Collection_DB/issues",
         "Source": "https://github.com/Kuuuube/osu_MapID_to_Collection_DB",
     },
-    version=VERSION,
+    version=about["__version__"],
 )
