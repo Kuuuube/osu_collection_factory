033965c5a70bea0f9ec36a24a02e39bacc4e33a6
numpee <kmanprovo@gmail.com>
2021-12-07 04:29:54 -0700

    Initial rewrite

--- mapID_to_collection_DB/set_id_to_map_ids.py
+++ mapID_to_collection_DB/set_id_to_map_ids.py
@@ -1,13 +1,14 @@
+from typing import Any
+
 from util import get_json_response
 
 # TODO cache setID -> mapIDs as a local db to speed up lookups
 
 
 # Writes all map IDs from set ID to list.txt
-def set_id_list_to_map_id_list(set_ids: set, api_key):
-    file_path = "list.txt"
-
+def set_id_list_to_map_id_list(set_ids: set, api_key) -> set:
     url = "https://osu.ppy.sh/api/get_beatmaps"
+    ids = set()
     for set_id in set_ids:
         payload = {
             'k': api_key,
@@ -15,7 +16,26 @@ def set_id_list_to_map_id_list(set_ids: set, api_key):
             'limit': 100,
         }
 
-        beatmap_json = get_json_response(url=url, payload=payload)
+        beatmap_json: dict[str | Any] | None = None
+        try:
+            beatmap_json = get_json_response(url=url, payload=payload)
+        except Exception as e:
+            print("An exception occurred in set_id_list_to_map_id_list:\n"
+                  f"{e}")
+
+            # noinspection PyUnusedLocal
+            user_input = None
+            while (user_input := input(f"Create collection from {len(ids)} sets? (y/n)")) not in ('y', 'n'):
+                print(f"Invalid input: {user_input}")
+
+            if user_input == 'y':
+                return ids
+
+            else:
+                quit(1)
+
+        if beatmap_json is None:
+            raise Exception("JSON response was received as None")
 
         beatmap_ids = []
         for beatmap in beatmap_json:
@@ -24,9 +44,11 @@ def set_id_list_to_map_id_list(set_ids: set, api_key):
             if beatmap_id is not None:
                 beatmap_ids.append(beatmap_id)
 
-        # Write map IDs after getting theme
-        with open(file_path, 'a') as beatmap_id_dump:
-            for beatmap_id in beatmap_ids:
-                beatmap_id_dump.write(beatmap_id + "\n")
+            else:
+                print(f"Beatmap set {set_id}: returned with an invalid id")  # TODO log this
 
-            print(f"SetID - {set_id}:\n - {beatmap_ids}")  # TODO log this
+        ids.add(*beatmap_ids)
+
+        print(f"SetID - {set_id}:\n - {beatmap_ids}")  # TODO log this
+
+    return ids
