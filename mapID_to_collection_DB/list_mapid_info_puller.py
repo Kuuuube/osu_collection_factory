317b028a4bd92432102534a076948dce3ea8891f
numpee <kmanprovo@gmail.com>
2021-12-07 04:30:55 -0700

    Initial rewrite

--- mapID_to_collection_DB/list_mapid_info_puller.py
+++ mapID_to_collection_DB/list_mapid_info_puller.py
@@ -1,45 +1,35 @@
 import json
-import requests
-import re
-import time
 import subprocess
 
-def id_to_db(api_key, collection_path):
+from pathlib import Path
 
-    collection_path_no_extension_1 = re.sub(".*(\\\|\\\\)", "", collection_path)
-    collection_path_no_extension_2 = re.sub("\..*", "", collection_path_no_extension_1)
-    filepath = 'CollectionCSVtoDB\\' + collection_path_no_extension_2 + '.csv'
+from mapID_to_collection_DB import util
 
-    with open(filepath, 'w') as beatmap_info_file:
-        beatmap_info_file.close()
-    
-    with open("list.txt", "r") as beatmapIDListRaw:
-        	beatmapIDListLines = beatmapIDListRaw.readlines()
-    beatmapIDList = list(map(str.strip, beatmapIDListLines))
 
-    for element in beatmapIDList:
-        beatmapid = element
-        url = "https://osu.ppy.sh/api/get_beatmaps"
+# TODO cache the id - hash pairs locally
+
+def id_to_db(map_ids: set, api_key: str):
+    with open("settings.json", "r") as f:
+        data = json.load(f)
+
+    filepath = Path(data["output_collection_path"]).joinpath(data["output_collection_name"] + '.csv')
+
+    url = "https://osu.ppy.sh/api/get_beatmaps"
+    for map_id in map_ids:
         payload = {
             'k': api_key,
-            'b': beatmapid,
+            'b': map_id,
             'type': 'id',
             'limit': 100,
         }
-        if len(beatmapid) > 0:
-            r = requests.get(url, params=payload)
-            beatmap = json.loads(r.text)
-            beatmap_regex = re.search('(?<="file_md5": ").*?(?=")',json.dumps(beatmap))
-            if beatmap_regex != None:
-                print ("ID: " + beatmapid + " MD5: " + beatmap_regex.group(0))
-    
-                with open(filepath, 'a') as beatmap_info_file:
-                    beatmap_info_file.writelines([",,"])
-                    beatmap_info_file.writelines([beatmap_regex.group(0)])
-                    beatmap_info_file.writelines(["\n"])
 
-            time.sleep(1)
-        if beatmapid == beatmapIDList[-1]:
-            beatmap_info_file.close()
-            subprocess.check_call([r"CollectionCSVtoDB\CollectionCSVtoDB.exe", filepath, collection_path])
-            break
+        if len(map_id) > 0:
+            beatmap_json = util.get_json_response(url, payload)
+
+            with open(filepath, 'a') as f:
+                f.write(beatmap_json["file_md5"] + "\n")
+
+            print(f"ID: {map_id} MD5: {beatmap_json['file_md5']}")  # TODO log this
+
+    subprocess.check_call([r"CollectionCSVtoDB\CollectionCSVtoDB.exe", filepath,
+                           Path("output_collection_path").joinpath(data["output_collection_name"] + ".db")])
