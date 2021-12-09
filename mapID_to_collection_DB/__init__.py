010219e5e3cacdb367c0b37d989963394d7f9090
numpee <kmanprovo@gmail.com>
2021-12-09 01:41:35 -0700

    Corrected imports

--- mapID_to_collection_DB/__init__.py
+++ mapID_to_collection_DB/__init__.py
@@ -2,12 +2,10 @@ import json
 
 from pathlib import Path
 
-from parser import parse_file
-from list_mapid_info_puller import id_to_db
+from parse_ids_from_file import parse_file
 from md5_conversion import convert_md5s_to_db
 from osu_collector_dumper import osu_collector_dump, osu_collector_dump_with_filter
-from util import get_json_response, get_osu_api_json_response,\
-    change_default_collection_output_name, change_default_collection_output_path
+from util import get_json_response, change_default_collection_output_name, change_default_collection_output_path
 
 try:
     import requests
